c91c9388ac33f8ee87d0151620ab45c690d7ea9e
Sihle-06 <sihlentshalintshali06@gmail.com>
2024-12-23 14:15:10 +0200

    product details added with routerlinks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { AuthPageComponent } from './pages/buyer/auth-page/auth-page.component';
 import { RegisterPageComponent } from './pages/buyer/register-page/register-page.component';
 import { RunnerListPageComponent } from './pages/buyer/runner-list-page/runner-list-page.component';
 import { RunnerProfilePageComponent } from './pages/buyer/runner-profile-page/runner-profile-page.component';
+import { ProductDetailsComponent } from './pages/buyer/product-details/product-details.component';
+
 
 export const routes: Routes = [
     {path: 'buyer-login', component: AuthPageComponent},
     {path: 'buyer-register', component: RegisterPageComponent},
     {path: 'runner-list', component: RunnerListPageComponent},
-    {path: 'runnerProfile', component:RunnerProfilePageComponent}
+    {path: 'runnerProfile', component:RunnerProfilePageComponent},
+    {path: 'productDetails', component: ProductDetailsComponent}
     
 ];
