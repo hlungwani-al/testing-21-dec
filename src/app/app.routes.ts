a9cb22118362c38e6fc6abc9626fe42da17ccb54
Sihle-06 <sihlentshalintshali06@gmail.com>
2024-12-23 12:33:10 +0200

    runner profile with the routerlink in the runner list, app.routes updated as well

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AuthPageComponent } from './pages/buyer/auth-page/auth-page.component';
 import { RegisterPageComponent } from './pages/buyer/register-page/register-page.component';
 import { RunnerListPageComponent } from './pages/buyer/runner-list-page/runner-list-page.component';
+import { RunnerProfilePageComponent } from './pages/buyer/runner-profile-page/runner-profile-page.component';
 
 export const routes: Routes = [
     {path: 'buyer-login', component: AuthPageComponent},
     {path: 'buyer-register', component: RegisterPageComponent},
-    {path: 'runner-list', component: RunnerListPageComponent}
+    {path: 'runner-list', component: RunnerListPageComponent},
+    {path: 'runnerProfile', component:RunnerProfilePageComponent}
     
 ];
--- src/app/pages/buyer/runner-list-page/runner-list-page.component.ts
+++ src/app/pages/buyer/runner-list-page/runner-list-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-runner-list-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './runner-list-page.component.html',
   styleUrl: './runner-list-page.component.css'
 })
