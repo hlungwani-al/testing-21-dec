7b35172a95289084796b6982a297f3f56028ba94
Hlungwani-al <Hlungwani.al6@gmail.com>
2024-12-24 17:03:29 +0200

    runner auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { RegisterPageComponent } from './pages/buyer/register-page/register-page
 import { RunnerListPageComponent } from './pages/buyer/runner-list-page/runner-list-page.component';
 import { RunnerProfilePageComponent } from './pages/buyer/runner-profile-page/runner-profile-page.component';
 import { ProductDetailsComponent } from './pages/buyer/product-details/product-details.component';
+import { RunnerRegisterPageComponent } from './pages/runner/runner-register-page/runner-register-page.component';
+import { RunnerAuthPageComponent } from './pages/runner/runner-auth-page/runner-auth-page.component';
 
 
 export const routes: Routes = [
@@ -11,6 +13,10 @@ export const routes: Routes = [
     {path: 'buyer-register', component: RegisterPageComponent},
     {path: 'runner-list', component: RunnerListPageComponent},
     {path: 'runnerProfile', component:RunnerProfilePageComponent},
-    {path: 'productDetails', component: ProductDetailsComponent}
-    
+    {path: 'productDetails', component: ProductDetailsComponent},
+
+    //runner path
+    {path: 'runner-login', component: RunnerAuthPageComponent},
+    {path: 'runner-register', component: RunnerRegisterPageComponent}
+
 ];
---
+++ src/app/pages/runner/runner-register-page/runner-register-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-runner-register-page',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './runner-register-page.component.html',
+  styleUrl: './runner-register-page.component.css'
+})
+export class RunnerRegisterPageComponent {
+
+}
