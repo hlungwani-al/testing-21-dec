5e94f57344bae496eec3361ee6449e0b086a10a6
Sihle-06 <sihlentshalintshali06@gmail.com>
2025-01-28 21:01:28 +0200

    sign up page with change password

--- src/app/pages/buyer/auth-page/auth-page.component.ts
+++ src/app/pages/buyer/auth-page/auth-page.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
+import { ModalService } from '../../../services/modal.service'; 
 
 @Component({
   selector: 'app-auth-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ForgotPasswordComponent],
   templateUrl: './auth-page.component.html',
   styleUrl: './auth-page.component.css'
 })
 export class AuthPageComponent {
+
+  // constructor(private modalService: ModalService) { }
+
+  // openForgotPasswordModal() {
+  //   this.modalService.openForgotPasswordModal();  // Open the modal via the service
+  // }
 
 }
