49cc517090e6c71d00c4c8cf9c5cd94ff4765677
Sihle-06 <sihlentshalintshali06@gmail.com>
2025-01-29 14:50:35 +0200

    runner order details

--- src/app/pages/runner/order-details-page/order-details-page.component.ts
+++ src/app/pages/runner/order-details-page/order-details-page.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-order-details-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order-details-page.component.html',
   styleUrl: './order-details-page.component.css'
 })
 export class OrderDetailsPageComponent {
 
+  
+  selectedTab: number = 0;
+
+  selectTab(index: number): void {
+    this.selectedTab = index;
+  }
 }
