a1781a15efd22f367d871112a56ca6208e24c73f
Sihle-06 <sihlentshalintshali06@gmail.com>
2025-01-29 09:21:55 +0200

    signup page and auth routing

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@angular/core';
 import { ForgotPasswordComponent } from '../pages/buyer/forgot-password/forgot-password.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  constructor(private modalService: NgbModal) { }
-
-  openForgotPasswordModal() {
-    this.modalService.open(ForgotPasswordComponent);
-  }
+  
 }
