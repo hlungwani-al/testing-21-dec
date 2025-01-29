dde828d55eb894e8259ec23ada9896bd3cd83585
Sihle-06 <sihlentshalintshali06@gmail.com>
2025-01-29 16:24:51 +0200

    order pages and fixed root style in components

--- src/app/pages/runner/order-details-page/order-details-page.component.ts
+++ src/app/pages/runner/order-details-page/order-details-page.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../components/navbar/navbar.component";
+import { LandingPageComponent } from "../../../components/landing-page/landing-page.component";
 
 @Component({
   selector: 'app-order-details-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent, LandingPageComponent],
   templateUrl: './order-details-page.component.html',
   styleUrl: './order-details-page.component.css'
 })
